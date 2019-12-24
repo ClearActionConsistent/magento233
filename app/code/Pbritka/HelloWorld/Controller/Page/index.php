b4b358f62128b303dec1a7a60978b348fb578eb1
Trung Nguyen <trung.nguyenchi@brightstart.com>
2019-12-24 15:23:45 +0700

    add block to module

--- app/code/Pbritka/HelloWorld/Controller/Page/index.php
+++ app/code/Pbritka/HelloWorld/Controller/Page/index.php
@@ -4,8 +4,18 @@ namespace Pbritka\HelloWorld\Controller\Page;
 
 class Index extends \Magento\Framework\App\Action\Action 
 {
+    protected $_pageFactory;
+
+    public function __construct(\Magento\Framework\View\Result\PageFactory $pageFactory,
+    \Magento\Framework\App\Action\Context $context)
+    {
+        $this-> _pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        echo 'Hello World!';
+        $page = $this->_pageFactory->create();
+        return $page;
     }
 }
